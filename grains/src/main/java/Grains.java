7678e1890c647cd6222324981fd550ddddb71df6
Abhishek Gokavarapu <iamabhishekgokavarapu@gmail.com>
2024-01-30 22:56:00 +0530

    Grains Solution

--- grains/src/main/java/Grains.java
+++ grains/src/main/java/Grains.java
@@ -1,13 +1,27 @@
 import java.math.BigInteger;
+import java.util.HashMap;
 
 class Grains {
 
     BigInteger grainsOnSquare(final int square) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if(square < 1 || square > 64) {
+            throw new IllegalArgumentException("square must be between 1 and 64");
+        }
+        HashMap<Integer, BigInteger> resultMap = fillChessSquaresWithGrains();
+        return resultMap.get(square);
     }
 
     BigInteger grainsOnBoard() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        HashMap<Integer, BigInteger> resultMap = fillChessSquaresWithGrains();
+        return resultMap.values().stream().reduce(BigInteger.ZERO, BigInteger::add);
     }
 
+    private HashMap<Integer, BigInteger> fillChessSquaresWithGrains(){
+        HashMap<Integer, BigInteger> resultMap = new HashMap<>();
+        for (int chessSquareNumber = 1; chessSquareNumber < 65; chessSquareNumber++) {
+            BigInteger numberOfGrains = chessSquareNumber == 1 ? BigInteger.ONE : resultMap.get(chessSquareNumber - 1).multiply(BigInteger.TWO);
+            resultMap.put(chessSquareNumber,numberOfGrains);
+        }
+        return resultMap;
+    }
 }
