bd2a8675aafea9a9878ac48bc35f09bc2cdfbd33
Abhishek Gokavarapu <iamabhishekgokavarapu@gmail.com>
2024-01-30 11:43:37 +0530

    Darts Solution

--- darts/src/main/java/Darts.java
+++ darts/src/main/java/Darts.java
@@ -1,5 +1,15 @@
 class Darts {
     int score(double xOfDart, double yOfDart) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        double distanceSquared = xOfDart * xOfDart + yOfDart * yOfDart;
+
+        if (distanceSquared <= 1) {
+            return 10;
+        } else if (distanceSquared <= 25) {
+            return 5;
+        } else if (distanceSquared <= 100) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
